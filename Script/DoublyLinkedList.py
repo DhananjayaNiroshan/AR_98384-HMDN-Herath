14a11838425ef84d1832d8c6e73e3eadab7512e4
Dhananjaya Niroshan <91305390+DhananjayaNiroshan@users.noreply.github.com>
2022-02-26 07:21:08 -0800

    print backword list

--- Script/DoublyLinkedList.py
+++ Script/DoublyLinkedList.py
@@ -24,3 +24,14 @@ class DoublyLinkedList:
             while current is not None:  
                 print(current.getData(), end=" ")  
                 current = current.getNext() 
+    
+    def printBackwardList(self):
+        if self.head is None:  
+            print("Empty Doubly Linked List")
+        else:
+            current = self.head
+            while current.getNext() is not None:  
+                current = current.getNext()  
+            while current is not None:  
+                print(current.getData(), end=" ")  
+                current = current.getPrev()  
