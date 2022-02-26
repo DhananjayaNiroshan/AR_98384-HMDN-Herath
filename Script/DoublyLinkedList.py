76df3b948a6c0de7abadef036e394939e0a4a923
Dhananjaya Niroshan <91305390+DhananjayaNiroshan@users.noreply.github.com>
2022-02-26 07:25:43 -0800

    add node in pos

--- Script/DoublyLinkedList.py
+++ Script/DoublyLinkedList.py
@@ -35,7 +35,7 @@ class DoublyLinkedList:
             while current is not None:  
                 print(current.getData(), end=" ")  
                 current = current.getPrev()  
-                
+
     def addNodeBeginning(self, data):
         newNode = Node(data)
         newNode.setData(data)
@@ -47,3 +47,38 @@ class DoublyLinkedList:
             newNode.setPrev(None)  
             self.head = newNode  
         self.length += 1 
+    
+    def addNodeEnd(self, data):
+        newNode = Node(data)
+        newNode.setData(data)
+        if self.head is None:
+            self.head = newNode
+        else:
+            current = self.head
+            while current.getNext() is not None:  
+                current = current.getNext()  
+            current.setNext(newNode)  
+            newNode.setPrev(current)  
+            newNode.setNext(None)  
+        self.length += 1
+    def addNodeInPos(self, pos, data):
+       
+        if pos > self.length - 1 or pos < 0:
+            return None
+        elif pos == self.length - 1:  
+            self.addNodeEnd(data)  
+        elif pos == 0:  
+            self.addNodeBeginning(data)  
+        else:
+            newNode = Node(data)
+            newNode.setData(data)
+            current = self.head
+            count = 0
+            while count != pos - 1:  
+                current = current.getNext()  
+                count += 1
+            newNode.setPrev(current)  
+            newNode.setNext(current.getNext())  
+            current.getNext().setPrev(newNode)  
+            current.setNext(newNode)  
+        self.length += 1
